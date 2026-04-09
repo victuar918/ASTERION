4b5984f8cb013e047fff77aff34c96f1b3f79023
victuar918 <victuar918@gmail.com>
2026-04-09 21:22:05 +0900

    Add new API endpoints for order management

--- js/api_contract.js
+++ js/api_contract.js
@@ -42,6 +42,10 @@ var A = Object.freeze({
   SAVE_STRUCTURE_TYPE   : "saveStructureType",
   // forwarding.html
   UPDATE_FORWARDING     : "updateForwarding",
+  // neworder.html
+  GET_ALL_REG_ROWS        : "getAllRegRows",
+  CREATE_CODE_AND_REGISTER: "createCodeAndRegister",
+  RECREATE_PDF            : "recreatePdf",
   // 공통
   GET_INVENTORY         : "getInventory",
   COMPACT_SHEET         : "compactSheet",
